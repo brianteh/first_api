f381098933ac3936e83eee80ed3dab83e01f50ee
teh <brianteh007@gmail.com>
2021-12-10 18:28:06 +0800

    added error handling

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ const morgan = require('morgan');
 const product = require('./api/routes/product');
 const order = require('./api/routes/order');
 
+
+// THESE ARE ALL MIDDLEWARES
 app.use(morgan('dev'));//console log requests details
 app.use('/product',product);
 app.use('/order',order);
 
-app.use('/',(req,res,next)=>{
-    res.status(200).json({
-        message:"Hi this is an api! HAHAHA"
+// if the requests arent successful,these will run
+app.use((req,res,next)=>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message
+        }
     });
 });
 
