858b73cb0613a504aa3200ea14aa38bb7ba5627a
teh <brianteh007@gmail.com>
2021-12-14 00:18:16 +0800

    added order

--- api/models/order.js
+++ api/models/order.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    orders_name: {type: String, required : true},
-    orders_price: {type: Number, required : true}
+    product_id:{type: mongoose.Schema.Types.ObjectId, ref: 'Product' , required: true},
+    quantity: {type: Number, default: 1}
 });
 
 module.exports = mongoose.model('Order', orderSchema);
